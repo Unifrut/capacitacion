cae2a41fed3751616adbdef9c9eb97ded4b4ba59
Unifrut <grupounifrut@gmail.com>
2022-11-10 22:17:42 +0000

    [ADD] Basic Module

---
+++ space/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name':'Space_Mission',
+    'summary':"""test""",
+    'description':"""
+           Hello world
+        """,
+    'author':'Unifrut',
+    'website':'https://www.unifrut.com',
+    'category':'Entertainment',
+    'version':'0.1',
+    'depends':['base'],
+    'data':[
+        
+    ],
+    'demo': [
+        #'demo/cohete.xml',
+    ],
+    'license':'OPL-1'
+}
