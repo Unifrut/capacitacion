88cd5461907aef9be930e16b0e3af9f615a6f1d4
Unifrut <grupounifrut@gmail.com>
2022-11-10 14:56:41 +0000

    [ADD] Basic Model

---
+++ biblioteca/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+{
+    'name':'Biblioteca',
+    'summary':"""App de pruebas Odoo""",
+    'description':"""
+            App diseñada como modelo de pruebas
+        """,
+    'author':'Unifrut',
+    'website':'https://www.unifrut.com',
+    'category':'Entrenamiento',
+    'version':'0.1',
+    'depends':['base'],
+    'data':[
+       # 'security/biblioteca_security.xml',
+       # 'security/ir.model.access.csv',
+    ],
+    'demo':[
+      #  'demo/biblioteca_demo.xml',
+    ],
+    'license':'OPL-1'
+}
