1d7080ce1564a810d5dffafa9af9314409343d4e
Unifrut <grupounifrut@gmail.com>
2022-11-15 19:01:23 +0000

    [Day 5] Advanced ORM

--- biblioteca/models/libro.py
+++ biblioteca/models/libro.py
@@ -16,3 +16,28 @@ class Libro(models.Model):
     año = fields.Integer(string='Año')
     isbn = fields.Integer(string='ISBN')
     active = fields.Boolean(string='Active', default=True)
+    
+    ubicacion = fields.Selection(string='Ubicacion',
+                            selection=[('bA', 'Bloque A'),
+                                       ('bB', 'Bloque B'),
+                                       ('bC', 'Bloque C'),
+                                       ('bD', 'Bloque D'),
+                                       ('bE', 'Bloque E')],
+                            copy=False)
+    precio = fields.Float(string='Precio', default=0.00)
+    paginas = fields.Integer(string='No Paginas', default=0)
+    
+    """
+    @api.onchange('precio')
+    def _onchange_precio(self):
+        if self < 0.00:
+            raise UserError(_('El precio no puede ser negativo.'))
+       """     
+    @api.onchange('isbn')
+    def _onchange_isbn(self): 
+         if (len(self) >10):
+            raise exceptions.ValidationError("El isbn no debe de pasar los 10 caracteres")
+   
+   
+
+       
